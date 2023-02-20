79285d696319a248e80d3fa0e6b380d4f4fd122f
Scott Cain <scott+git@scottcain.net>
2023-02-20 09:44:08 -0800

    missed a place a name had to be changed

--- htVariantColor_plugin.js
+++ htVariantColor_plugin.js
@@ -40,7 +40,7 @@
 
   // the plugin will be included in both the main thread and web worker, so
   // install plugin to either window or self (webworker global scope)
-  ;(typeof self !== 'undefined' ? self : window).JBrowsePluginVariantColorPlugin =
+  ;(typeof self !== 'undefined' ? self : window).JBrowsePluginHTVariantColorPlugin =
     {
       default: Plugin,
     }
